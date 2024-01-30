69ce3f892824e2fa990dde0391795b74300c9a2b
alacosay <alacosay@mail.ru>
2024-01-30 13:01:35 +0300

    Added task 1-5 of 3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split('')
+    .map((char, i, arr) => {
+      if(char == '-') {
+        arr[i + 1] = arr[i + 1].toUpperCase();
+      } else return char;
+    })
+    .join('');
 }
