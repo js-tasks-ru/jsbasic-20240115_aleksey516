099985c7b976dcf83de6151ffc387afd0e14badc
alacosay <alacosay@mail.ru>
2024-02-03 23:18:34 +0300

    Added task 1-3 of 4-module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,5 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for(let i = 0; i < table.querySelectorAll('tr').length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  for(let tr of table.querySelectorAll('tbody>tr')) {
+    
+    if(tr.cells[3].dataset.available == 'true') {
+      tr.classList.add('available');
+    }
+    else if(tr.cells[3].dataset.available == 'false') {
+      tr.classList.add('unavailable');
+    } else {
+      tr.setAttribute('hidden', '');
+    }
+  
+    if(tr.cells[2].textContent == 'm') {
+      tr.classList.add('male');
+    } else {
+      tr.classList.add('female');
+    }
+  
+    if(tr.cells[1].textContent < 18) {
+      tr.style.cssText += 'text-decoration: line-through';
+    }
+  }
 }
