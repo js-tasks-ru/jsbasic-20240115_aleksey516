f956ac25b81538bd86583d6956b4abbc14f5d39a
alacosay <alacosay@mail.ru>
2024-02-09 15:48:53 +0300

    Added task 1-3 of 5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const text = document.querySelector('#text');
+  const button = document.querySelector('.toggle-text-button');
+  button.addEventListener('click', () => {
+    (text.hidden) ? text.hidden = false : text.hidden = true;
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,25 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const slidelWidth = document.querySelector('.carousel__slide').offsetWidth;
+  const carouselInner = document.querySelector('.carousel__inner');
+
+  const carouselArrowRight = document.querySelector(".carousel__arrow_right");
+  const carouselArrowLeft = document.querySelector(".carousel__arrow_left");
+
+  let counterSlidePos = 0;
+  
+  function carouselMove(event) {
+    if(event.target.closest('DIV') == carouselArrowRight) {
+      carouselInner.style.transform = `translateX(${-slidelWidth - counterSlidePos * slidelWidth}px)`;
+      counterSlidePos++;
+    } else if(event.target.closest('DIV') == carouselArrowLeft){
+      counterSlidePos--;
+      carouselInner.style.transform = `translateX(${-slidelWidth * counterSlidePos}px)`;
+    }
+    counterSlidePos == 0 ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
+    counterSlidePos == 3 ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
+  }
+
+  carouselArrowLeft.style.display = 'none';
+  carousel.addEventListener('click', (event) => carouselMove(event));
 }
