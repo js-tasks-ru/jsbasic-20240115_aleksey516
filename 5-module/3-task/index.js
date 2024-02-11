404747ef65c194898c1ea541b821eb33d91053c1
alacosay <alacosay@mail.ru>
2024-02-11 20:56:00 +0300

    Added task 1-3 of 5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,21 +1,21 @@
 function initCarousel() {
   const carousel = document.querySelector('.carousel');
-  const slidelWidth = document.querySelector('.carousel__slide').offsetWidth;
-  const carouselInner = document.querySelector('.carousel__inner');
-
   const carouselArrowRight = document.querySelector('.carousel__arrow_right');
   const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
-
   let counterSlidePos = 0;
   
   function carouselMove(event) {
-    if(event.target.closest('DIV') == carouselArrowRight) {
+    const carouselInner = document.querySelector('.carousel__inner');
+    const slidelWidth = document.querySelector('.carousel__slide').offsetWidth;
+
+    if(event.target.closest('.carousel__arrow_right')) {
       carouselInner.style.transform = `translateX(${-slidelWidth * (counterSlidePos + 1)}px)`;
       counterSlidePos++;
-    } else if(event.target.closest('DIV') == carouselArrowLeft){
+    } else if(event.target.closest('.carousel__arrow_left')) {
       counterSlidePos--;
       carouselInner.style.transform = `translateX(${-slidelWidth * counterSlidePos}px)`;
     }
+
     counterSlidePos == 0 ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
     counterSlidePos == 3 ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
   }
