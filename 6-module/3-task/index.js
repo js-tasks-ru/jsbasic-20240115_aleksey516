233939fc89bd10aa9898fc03a42be8d9f8d91d8e
alacosay <alacosay@mail.ru>
2024-02-18 15:49:03 +0300

    Added task 1-3 of 7-module

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -2,22 +2,22 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
   elem = null;
-  slides = [];
-  carouselInner = {};
-  arrowLeft = {};
-  arrowRight = {};
-  slidelWidth = 0;
-  slideQuant = 0;
-  counterSlidePos = 0;
+  #slides = [];
+  #carouselInner = {};
+  #arrowLeft = {};
+  #arrowRight = {};
+  #slidelWidth = 0;
+  #slideQuant = 0;
+  #counterSlidePos = 0;
 
   constructor(slides) {
-    this.slides = slides || this.slides;
-    this.elem = this.createRootCarusel(slides);
-    this.initCarousel();
-    this.render();
+    this.#slides = slides || this.#slides;
+    this.elem = this.#createRootCarusel(slides);
+    this.#initCarousel();
+    this.#render();
   }
 
-  createRootCarusel(slides) {
+  #createRootCarusel(slides) {
     return createElement(`
     <div class="carousel">
       <!--Кнопки переключения-->
@@ -29,13 +29,13 @@ export default class Carousel {
       </div>
   
       <div class="carousel__inner">
-        ${this.slides.map((slide) => this.createSlide(slide)).join('')}
+        ${this.#slides.map((slide) => this.#createSlide(slide)).join('')}
       </div>
     </div>
     `);
   }
 
-  createSlide(slide) {
+  #createSlide(slide) {
     return `
     <div class="carousel__slide" data-id=${slide.id}>
       <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
@@ -50,37 +50,37 @@ export default class Carousel {
     `;
   }
 
-  initCarousel() {
-    this.carouselInner = this.elem.querySelector('.carousel__inner');
-    this.arrowLeft = this.elem.querySelector('.carousel__arrow_left');
-    this.arrowRight = this.elem.querySelector('.carousel__arrow_right');
-    this.slideQuant = this.elem.querySelectorAll('.carousel__slide').length;
+  #initCarousel() {
+    this.#carouselInner = this.elem.querySelector('.carousel__inner');
+    this.#arrowLeft = this.elem.querySelector('.carousel__arrow_left');
+    this.#arrowRight = this.elem.querySelector('.carousel__arrow_right');
+    this.#slideQuant = this.elem.querySelectorAll('.carousel__slide').length;
 
-    this.arrowLeft.style.display = 'none';
-    this.elem.addEventListener('click', (event) => this.carouselMove(event));
+    this.#arrowLeft.style.display = 'none';
+    this.elem.addEventListener('click', (event) => this.#moveCarousel(event));
   }
 
-  carouselMove(event) {
-    this.slidelWidth = this.elem.querySelector('.carousel__slide').offsetWidth;
+  #moveCarousel(event) {
+    this.#slidelWidth = this.elem.querySelector('.carousel__slide').offsetWidth;
   
     if(event.target.closest('.carousel__arrow_right')) {
-      this.carouselInner.style.transform = `translateX(${-this.slidelWidth * (this.counterSlidePos + 1)}px)`;
-      this.counterSlidePos++;
+      this.#carouselInner.style.transform = `translateX(${-this.#slidelWidth * (this.#counterSlidePos + 1)}px)`;
+      this.#counterSlidePos++;
     } else if(event.target.closest('.carousel__arrow_left')) {
-      this.counterSlidePos--;
-      this.carouselInner.style.transform = `translateX(${-this.slidelWidth * this.counterSlidePos}px)`;
+      this.#counterSlidePos--;
+      this.#carouselInner.style.transform = `translateX(${-this.#slidelWidth * this.#counterSlidePos}px)`;
     }
-    this.counterSlidePos == 0 ? this.arrowLeft.style.display = 'none' : this.arrowLeft.style.display = '';
-    this.counterSlidePos == (this.slideQuant - 1) ? this.arrowRight.style.display = 'none' : this.arrowRight.style.display = '';
+    this.#counterSlidePos == 0 ? this.#arrowLeft.style.display = 'none' : this.#arrowLeft.style.display = '';
+    this.#counterSlidePos == (this.#slideQuant - 1) ? this.#arrowRight.style.display = 'none' : this.#arrowRight.style.display = '';
   }
     
-  render() {
+  #render() {
     this.elem.addEventListener('click', (event) => {
       if(event.target.closest('.carousel__button')) {
-        this.addCustomEvent(event);}})
+        this.#addCustomEvent(event);}})
   }
 
-  addCustomEvent(event) {
+  #addCustomEvent(event) {
     const slideId = event.target.closest('.carousel__slide').dataset.id;
     const _customEvent = new CustomEvent("product-add", {
                           detail: slideId,
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,84 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
+  elem = null;
+  #steps = 0;
+  #value = 0;
+  #thumb = null;
+  #progress = null;
+  #sliderValue = null;
+  #valuePercents = 0;
+  #segments = 0;
+
   constructor({ steps, value = 0 }) {
+    this.#steps = steps;
+    this.#value = value;
+    this.elem = this.#createRootSlider() || this.elem;
+    this.#valuePercents = this.#value / (this.#steps - 1) * 100;
+    this.#initSlider();
+    this.#renderSliderOnClick();
   }
+
+  #createRootSlider() {
+    let spanArr = [];      
+    for(let i=0; i<this.#steps; i++) {
+      spanArr.push(`<span data-id="${i}"></span>`);
+    }
+    return new createElement(`
+      <div class="slider">
+        <div class="slider__thumb" style="left: ${this.#valuePercents}%;">
+          <span class="slider__value">${this.#value}</span>
+        </div>
+          <div class="slider__progress" style="width: ${this.#valuePercents}%;"></div>
+          <div class="slider__steps">
+          ${spanArr.join('')}
+        </div>
+      </div>
+    `);
+  }
+
+  #initSlider() {
+    this.#thumb = this.elem.querySelector('.slider__thumb');
+    this.#progress = this.elem.querySelector('.slider__progress');
+    this.#sliderValue = this.elem.querySelector('.slider__value');
+    this.#segments = this.#steps - 1;
+    this.elem.querySelector(`[data-id="${this.#value}"]`).classList.add('slider__step-active');
+  }
+
+  #renderSliderOnClick() {
+    this.elem.addEventListener('click', (event) => {
+      if(event.target.closest('.slider')) {
+        let left = event.clientX - this.elem.getBoundingClientRect().left;
+        let leftRelative = left / this.elem.offsetWidth;
+        let approximateValue = leftRelative * this.#segments;
+        let _value = Math.round(approximateValue);
+        if(this.#value != _value) {
+          this.#value = _value;
+          this.#moveSlider();
+          this.#addCustomEvent();
+        }
+      }
+    });
+  }
+
+  #moveSlider() {
+    this.#valuePercents = this.#value / this.#segments * 100;
+    if(this.elem.querySelector('.slider__step-active')) {
+        this.elem.querySelector('.slider__step-active').classList.remove('slider__step-active');
+    }
+    this.elem.querySelector(`[data-id="${this.#value}"]`).classList.add('slider__step-active');
+    this.#sliderValue.textContent = this.#value;
+    this.#thumb.style.left = `${this.#valuePercents}%`;
+    this.#progress.style.width = `${this.#valuePercents}%`;
+  }
+
+  #addCustomEvent() {
+    let _customEvent =
+      new CustomEvent(
+          'slider-change',
+          {detail: this.#value,
+          bubbles: true}
+      );
+    this.elem.dispatchEvent(_customEvent);
+  } 
 }
